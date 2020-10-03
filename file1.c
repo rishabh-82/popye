d39bcee3bfe8e1f20dc34bf225b7a6d56c6c32c1
Rishabh Singh <rishabh.s100101@gmail.com>
2020-10-03 22:11:42 +0530

    added files

--- file1.c
+++ file1.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	printf("Hello world! ererwwerwerwr");
+	printf("Hello world! ererwwerwerwr popye");
 	return 0;
 // comment
 }
--- file2.c
+++ file2.c
@@ -2,3 +2,4 @@ afgsgfjkasgfgasjfgakfj
 sasd
 sadasda
 
+popat
